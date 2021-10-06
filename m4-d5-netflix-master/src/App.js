1f37c2338fcf63bf16e1416f990b9c85bd3e8cc4
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-06 14:18:45 +0100

    Added movie details component and installed router

--- m4-d5-netflix-master/src/App.js
+++ m4-d5-netflix-master/src/App.js
@@ -4,17 +4,22 @@ import NavigationBar from './components/NavigationBar';
 import GenresMenu from './components/GenresMenu';
 import ShowMovies from './components/ShowMovies';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <NavigationBar />
+      <Router>
+        <NavigationBar />
+        <Switch>
       <GenresMenu />
       <ShowMovies searchBar="true" query="Harry Potter" />
       <ShowMovies searchBar="false" query="Lord of The Rings" />
       <ShowMovies searchBar="false" query="Star Wars" />
-      <Footer />
+        <Footer />
+        </Switch>
+      </Router>
     </div>
   );
 }
